4e85f020a1016d3678df70352072cd642119605b
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-08-12 18:00:42 +0530

    Added:Welcome Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CensusAnalyserTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
